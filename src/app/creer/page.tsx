3afc47ab29dd9055157f7d799b423011cb250c3b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-19 13:20:09 +0200

    créer une epingle

--- src/app/creer/page.tsx
+++ src/app/creer/page.tsx
@@ -5,6 +5,12 @@ const CreatePinPage = () => {
   return (
     <section className="w-full border-t border-gray-400 mt-8 flex items-start h-[calc(100vh-88px)]">
         <SidebarLeft />    
+
+        <div className='w-full flex items-center justify-center'>
+            <div className='flex items-center border-b border-gray-400 w-full p-6'>
+                <h2 className='text-xl font-medium'>Créer une Épingle</h2>
+            </div>
+        </div>
     </section>
   )
 }
--- src/components/createPage/SidebarLeft.tsx
+++ src/components/createPage/SidebarLeft.tsx
@@ -5,7 +5,7 @@ import { ButtonCreateNewPin } from "./ButtonCreateNewPin"
 
 export const SidebarLeft = () => {
   const [isOpen, setIsOpen] = useState(false)
-  return <div className={`${isOpen ? "w-[350px]" : "w-[82px]"} border-r border-gray-400 h-full flex flex-col items-center py-5 gap-9`}>
+  return <div className={`${isOpen ? "w-[400px]" : "w-[85px]"} border-r border-gray-400 h-full flex flex-col items-center py-5 gap-9`}>
     <div className="flex items-center justify-between w-full px-5">
         {isOpen && <p className="text-xl font-medium">Brouillons d'Épingle</p>}
         <ChevronsRight size={32} className={`cursor-pointer hover:text-blue-400 duration-75 ${isOpen ? "rotate-180" : ""}`} onClick={() => setIsOpen(!isOpen)} />
