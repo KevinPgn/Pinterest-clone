2d5d5c08b580d284d6939e10dc6fb15cc1fd0f2b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-19 13:17:33 +0200

    btn

--- src/components/createPage/ButtonCreateNewPin.tsx
+++ src/components/createPage/ButtonCreateNewPin.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { Button } from "../ui/button"
-
+import {useRouter} from "next/navigation"
 export const ButtonCreateNewPin = () => {
+  const router = useRouter()
   return <>
-    <Button className="w-[90%] rounded-full">Créer une nouvelle Épingle</Button>
+    <Button className="w-[90%] rounded-full" onClick={() => router.push("/creer")}>Créer une nouvelle Épingle</Button>
   </>
 }
