7c83ca23d52893893f61c859eb16d7599eca5601
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-19 13:16:59 +0200

    epingle

--- src/app/creer/page.tsx
+++ src/app/creer/page.tsx
@@ -1,8 +1,11 @@
+import { SidebarLeft } from '@/components/createPage/SidebarLeft'
 import React from 'react'
 
 const CreatePinPage = () => {
   return (
-    <div>CreatePinPage</div>
+    <section className="w-full border-t border-gray-400 mt-8 flex items-start h-[calc(100vh-88px)]">
+        <SidebarLeft />    
+    </section>
   )
 }
 
---
+++ src/components/createPage/ButtonCreateNewPin.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { Button } from "../ui/button"
+
+export const ButtonCreateNewPin = () => {
+  return <>
+    <Button className="w-[90%] rounded-full">Créer une nouvelle Épingle</Button>
+  </>
+}
