02b07bdc0cfda822d8983a4d06524b37e323b753
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-19 16:17:43 +0200

    link

--- src/components/accueil/HomeAccueil.tsx
+++ src/components/accueil/HomeAccueil.tsx
@@ -1,6 +1,7 @@
 import { GetPins } from "@/server/Actions"
 import { Button } from "../ui/button";
 import {Share, Ellipsis} from "lucide-react"
+import Link from "next/link"
 
 export async function HomeAccueil() {
   const pins = await GetPins()
@@ -9,17 +10,19 @@ export async function HomeAccueil() {
     <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4 mx-auto p-4">
       {pins.map((pin, index) => (
         <div key={index} className="mb-4 break-inside-avoid relative cursor-pointer group">
-          <img
-            src={pin.imageUrl}
-            alt={pin.imageUrl}
-            loading="lazy"
-            width={400}
-            height={600}
-            className="w-full rounded-lg"
-            style={{ height: 'auto' }}
-          />
-
-          <div className="hidden group-hover:block bg-black/50 absolute top-0 left-0 w-full h-full duration-75">
+            <Link href={`/pin/${pin.id}`}>
+              <img
+                src={pin.imageUrl}
+                alt={pin.imageUrl}
+                loading="lazy"
+                width={400}
+                height={600}
+                className="w-full rounded-lg"
+              style={{ height: 'auto' }}
+            />
+            </Link>
+            
+          <div className="hidden group-hover:block w-full h-full duration-75">
             <Button className="bg-red-500 absolute top-5 right-5 rounded-full hover:bg-red-600 py-6 text-md">Enregistrer</Button>
             <div className="absolute bottom-5 right-5 flex items-center gap-3">
               <div className="bg-white rounded-full p-2 hover:bg-gray-200 duration-75">
