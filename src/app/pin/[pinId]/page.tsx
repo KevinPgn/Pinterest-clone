bba724c7e452341bb272dbda82c1d9dc8ffb390d
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-21 13:25:37 +0200

    mises a jour

--- src/app/pin/[pinId]/page.tsx
+++ src/app/pin/[pinId]/page.tsx
@@ -3,20 +3,21 @@ import { BackToHome } from "@/features/pinPage/components/BackToHome"
 import { Pin as PinComponent } from "@/features/pinPage/components/Pin"
 
 interface PinPageProps {
-    params: {
+    params: Promise<{
         pinId: string
-    }
+    }>
 }
 
-const PinPage = async ({params}: PinPageProps) => {
-  const pin = await getPin(params.pinId)
-  
-  return (
-    <section className="max-w-[1000px] mx-auto mt-7">
-        <BackToHome />
-        <PinComponent pin={pin} />
-    </section>
-  )
+const PinPage = async (props: PinPageProps) => {
+    const params = await props.params;
+    const pin = await getPin(params.pinId)
+
+    return (
+      <section className="max-w-[1000px] mx-auto mt-7">
+          <BackToHome />
+          <PinComponent pin={pin} />
+      </section>
+    )
 }
 
 export default PinPage
