3fba3df34543cdf5e949c05a773d73245697d71b
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-21 13:11:11 +0200

    changement

--- src/components/navbar/Informations.tsx
+++ src/components/navbar/Informations.tsx
@@ -4,8 +4,8 @@ import { signIn } from "next-auth/react"
 
 export const Informations = ({currentSession}: {currentSession: any}) => {
   return <div className="flex items-center gap-2">
-    <Bell size={55} className="cursor-pointer hover:bg-gray-100 rounded-full duration-300 p-2"/>
-    <MessageCircleMore size={55} className="cursor-pointer hover:bg-gray-100 rounded-full duration-300 p-2"/>
+    <Bell size={40} className="cursor-pointer hover:bg-gray-100 rounded-full duration-300 p-2"/>
+    <MessageCircleMore size={40} className="cursor-pointer hover:bg-gray-100 rounded-full duration-300 p-2"/>
     {currentSession ? (
         <img src={currentSession.user.image} alt="profile" width={45} height={45} className="cursor-pointer hover:bg-gray-100 rounded-full duration-300 p-2"/>
     ) : (
