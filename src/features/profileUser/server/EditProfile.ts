3870de8186e94a4d0f180c1263aae0d9fe453bd9
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-21 17:18:27 +0200

    edit profil

--- src/features/profileUser/server/EditProfile.ts
+++ src/features/profileUser/server/EditProfile.ts
@@ -6,3 +6,31 @@ import { revalidatePath } from "next/cache"
 import {cache} from "react"
 import { getSession } from "@/components/utils/CacheSession"
 
+export const editProfileUser = authenticatedAction
+    .schema(z.object({
+        name: z.string().optional(),
+        image: z.string().optional(),
+    }))
+    .action(async ({parsedInput: {name, image}, ctx: {userId}}) => {
+        const session = await getSession()
+        const sessionUserId = session?.user?.id
+        
+        const user = await prisma.user.findUnique({
+            where: {id: userId}
+        })
+
+        if (!user) throw new Error("User not found")
+        if (sessionUserId !== userId) throw new Error("You are not authorized to edit this profile")
+        
+        await prisma.user.update({
+            where: {id: userId},
+            data: {
+                name,
+                image,
+            }
+        })
+
+        revalidatePath(`/${userId}`)
+        return user
+    })
+
