673a4bb758c3f79cf90de78ae908373cde7d0ab7
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-21 16:45:30 +0200

    user informations

--- src/features/profileUser/components/InformationsUser.tsx
+++ src/features/profileUser/components/InformationsUser.tsx
@@ -1,7 +1,10 @@
 import { Share, Ellipsis } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { getSession } from "@/components/utils/CacheSession";
 
-export const InformationsUser = ({userDetails}: {userDetails: any}) => {
+export const InformationsUser = async ({userDetails}: {userDetails: any}) => {
+  const session = await getSession();
+  const sessionUserId = session?.user?.id;
   return <div className="max-w-[650px] mx-auto flex flex-col mt-5">
     <div className="w-full h-[350px] bg-blue-500 rounded-[30px] relative">
       <img src={userDetails.image} alt="user image" className="w-[110px] absolute h-[110px] rounded-full bottom-[-50px] left-[50%] translate-x-[-50%] border-2 border-white object-cover" />
@@ -17,10 +20,16 @@ export const InformationsUser = ({userDetails}: {userDetails: any}) => {
 
     <div className="flex justify-center items-center gap-7 mt-7">
       <Share className="w-7 h-7 cursor-pointer" />
-      <div className="flex gap-3">
-        <Button variant="outline" className="rounded-full bg-gray-200 text-black text-md py-6 px-5 hover:bg-gray-100">Message</Button>
-        <Button variant="outline" className="rounded-full bg-[#E60023] text-white text-md py-6 px-5 hover:bg-[#E60023]/80 hover:text-white">S'abonner</Button>
-      </div>
+      {sessionUserId === userDetails.id ? (
+        <div>
+           <Button variant="outline" className="rounded-full bg-[#E60023] text-white text-md py-6 px-5 hover:bg-[#E60023]/80 hover:text-white">Modifier Profile</Button>
+        </div>
+      ): (
+        <div className="flex gap-3">
+          <Button variant="outline" className="rounded-full bg-gray-200 text-black text-md py-6 px-5 hover:bg-gray-100">Message</Button>
+          <Button variant="outline" className="rounded-full bg-[#E60023] text-white text-md py-6 px-5 hover:bg-[#E60023]/80 hover:text-white">S'abonner</Button>
+        </div>
+      )}
       <Ellipsis className="w-7 h-7 cursor-pointer" />
     </div>
   </div>
